311bb38676705c7ee456207dbf34cf6c180ea6e4
Štěpán Bechynský <stepan@bechynsky.cz>
2024-04-22 10:08:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Paní Doudová</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Paní Doudová Vás zdraví</h1>
+        <img src="doudova.jpg" alt="Paní Doudová v košíku" />
+    </body>
+</html>
